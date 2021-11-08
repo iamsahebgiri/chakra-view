f49b55c88f7667e7a180cf2eafad264c09bf7aad
Saheb Giri <iamsahebgiri@gmail.com>
2021-11-08 18:41:52 +0530

    revert: :fire: remove all code

--- components/Card.js
+++
@@ -1,8 +0,0 @@
-import { Box, useStyleConfig } from '@chakra-ui/react';
-
-export default function Card(props) {
-  const { variant, ...rest } = props;
-  const styles = useStyleConfig('Card', { variant });
-
-  return <Box __css={styles} {...rest} />;
-}
--- components/temp/getExampleUrl.js
+++
@@ -1,3 +0,0 @@
-export const getExampleUrl = (category, subCategory, template) => {
-  return `/templates/${category.id}/${subCategory.id}/${template.filename}`;
-};
